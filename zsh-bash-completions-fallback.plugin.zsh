4c7d27fc1c889c364d70cdd05f1990b535b9b1d1
Marco Trevisan (Treviño) <mail@3v1n0.net>
2020-05-20 13:42:21 +0200

    Hello zsh-bash-completions-fallback Plugin!
    This plugin is intended to use the bash completions when a zsh
    completion is not available.
    
    Make sure you load this after other plugins not to prevent their
    completions to be overridden by the bash ones.
    
    Not all the bash completions can work properly, as per missing compopt
    support.

---
+++ zsh-bash-completions-fallback.plugin.zsh
@@ -0,0 +1,25 @@
+our_path=${0:A:h}
+
+function _bash_completer {
+    cmd=${words[@]};
+    out=("${(@f)$(bash -c "source ${our_path}/bash-completions-getter.sh; get_completions '$cmd'")}");
+    compadd -a out
+}
+
+if ! [ -f /etc/bash_completion ] ||
+   ! [ -f /usr/share/bash-completion/bash_completion ]; then
+   return 1;
+fi
+
+_completed_commands=()
+for command completion in ${(kv)_comps:#-*(-|-,*)}; do
+    _completed_commands+=($command)
+done
+
+for i in /usr/share/bash-completion/completions/*; do
+    completion=$(basename $i);
+
+    if [[ ${_completed_commands[(ie)${completion}]} -gt ${#_completed_commands} ]]; then
+        compdef _bash_completer $completion;
+    fi
+done
